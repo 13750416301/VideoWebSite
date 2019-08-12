ece205cb0d420aa4288388d82c1238b57ae7ca12
caimb <caimb@123.com>
2019-08-12 17:15:05 +0800

    引入iView

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 // import Plyr from 'plyr'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 import VuePlyr from 'vue-plyr'
 import 'vue-plyr/dist/vue-plyr.css' // only if your build system can import css, otherwise import it wherever you would import your css.
 
+Vue.use(iView)
 Vue.use(VuePlyr)
 
 Vue.config.productionTip = false
